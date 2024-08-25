dfd0607777f2d1616409f26cdd0e230721ff3b74
mvstermind <zablockidawid1@gmail.com>
2024-08-26 00:04:25 +0200

    feat: i'm rusty af

--- main.go
+++ main.go
@@ -24,14 +24,32 @@ func main() {
 			log.Println(err)
 			return
 		}
+
+		fileState, err := os.ReadFile("test.txt")
+		if err != nil {
+			log.Println(err)
+		}
+
 		newTime := fileInfo.ModTime()
 
 		if newTime.After(oldTime) {
 			fmt.Println("This file has changed")
 			oldTime = newTime
+
+			newFileState, err := os.ReadFile("test.txt")
+			if err != nil {
+				fmt.Println("bruh this can't even happen")
+			}
+
+			for _, v := range newFileState {
+
+				if fileState[v] != newFileState[v] {
+					fmt.Printf("%v", v)
+				}
+
+			}
 		}
 		time.Sleep(200 * time.Millisecond)
 
 	}
 }
-
